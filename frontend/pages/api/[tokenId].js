4e12e62ad6b37204412c756154e5845267968eb4
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-01-18 10:43:07 +0530

    update

--- frontend/pages/api/[tokenId].js
+++ frontend/pages/api/[tokenId].js
@@ -1,9 +1,10 @@
+
 export default function handler(req, res) {
   // get the tokenId from the query params
   const tokenId = req.query.tokenId;
   // As all the images are uploaded on github, we can extract the images from github directly.
   const image_url =
-    "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+    "https://raw.githubusercontent.com/Virendra-khorwal/NFT-Collection/aa7ba86848886fa8246e39656e47fc644137ccb5/frontend/public/cryptodevs/";
   // The api is sending back metadata for a Crypto Dev
   // To make our collection compatible with Opensea, we need to follow some Metadata standards
   // when sending back the response from the api
